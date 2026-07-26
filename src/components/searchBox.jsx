d874faa2ebd5e07ff80fde88006fdca0b20db9c3
Princy Mishra <princyyy03@gmail.com>
2026-07-26 11:37:13 +0530

    Add searchBox component

---
+++ src/components/searchBox.jsx
@@ -0,0 +1,20 @@
+export default function SearchBox({ username, setUsername, onSearch, loading }) {
+  return (
+    <div className="input-group mb-3">
+      <input 
+        className="form-control"
+        type="text"
+        placeholder="Enter leetcode username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button 
+        className="btn btn-dark" 
+        type="button"
+        onClick={onSearch}
+      >
+        {loading ? 'Searching...' : 'Search'}
+      </button>
+    </div>
+  );
+}
